65f68d7a0a1454432936e3cda92da594267b4d99
Zachary Kramer <kramer.zachary.nau@gmail.com>
2016-04-18 20:20:11 -0700

    More fields, needs final implementation
    Radiobuttons, entry, and everything is anchored

--- graphSelection.py
+++ graphSelection.py
@@ -8,15 +8,14 @@ class GUI:
         # Generate window
         self.root = Tk()
         self.root.title("Generate Graphs")
-        self.frame = Frame(self.root)
-        self.root.maxsize(250, 180)
-        self.root.minsize(250, 180)
+        self.root.maxsize(200, 400)
+        self.root.minsize(200, 400)
 
         # ttk styles
         self.green_button = ttk.Style()
         self.green_button.map("TButton", foreground=[('pressed', 'white'), ('active', 'green')],  background=[('pressed', 'white'), ('active', 'white')])
 
-        # Checkbutton variables
+        # Checkbox variables
         self.a = IntVar()
         self.b = IntVar()
         self.c = IntVar()
@@ -26,47 +25,60 @@ class GUI:
         self.graphs = [self.a, self.b, self.c, self.d, self.e, self.f]
         self.selected = []
 
-        # Checkbuttons
-        self.graph_0 = ttk.Checkbutton(self.root, text="Employee information", variable=self.a, width=200, style="TCheckbutton")
-        self.graph_0.pack()
-        self.graph_1 = ttk.Checkbutton(self.root, text="Number of customers per hour", variable=self.b, width=200)
-        self.graph_1.pack()
-        self.graph_2 = ttk.Checkbutton(self.root, text="Daily customer statistics (multigraph)", variable=self.c, width=200)
-        self.graph_2.pack()
-        self.graph_3 = ttk.Checkbutton(self.root, text="Revenue (daily, monthly, yearly)", variable=self.d, width=200)
-        self.graph_3.pack()
-        self.graph_4 = ttk.Checkbutton(self.root, text="Items sold (daily, monthly, yearly)", variable=self.e, width=200)
-        self.graph_4.pack()
-        self.graph_5 = ttk.Checkbutton(self.root, text="Extra statistics", variable=self.f, width=200)
-        self.graph_5.pack()
+        # Radiobutton variable
+        self.date_type = IntVar()
+
+        # Entry variables
+        self.date_text = "MM-DD-YY"
+
+        # Checkboxes
+        self.graph_label = Label(self.root, text="Graph selection:", pady=10).pack(anchor=W)
+        self.graph_0 = ttk.Checkbutton(self.root, text="Employee information", variable=self.a, width=200, style="TCheckbutton").pack(anchor=W)
+        self.graph_1 = ttk.Checkbutton(self.root, text="Number of customers per hour", variable=self.b, width=200).pack(anchor=W)
+        self.graph_2 = ttk.Checkbutton(self.root, text="Daily customer statistics", variable=self.c, width=200).pack(anchor=W)
+        self.graph_3 = ttk.Checkbutton(self.root, text="Revenue", variable=self.d, width=200).pack(anchor=W)
+        self.graph_4 = ttk.Checkbutton(self.root, text="Items sold", variable=self.e, width=200).pack(anchor=W)
+        self.graph_5 = ttk.Checkbutton(self.root, text="Extra statistics", variable=self.f, width=200).pack(anchor=W)
+
+        # Radiobuttons
+        self.radio_label = Label(self.root, text="Data of graph(s)", pady=10).pack(anchor=W)
+        self.daily_button = ttk.Radiobutton(self.root, text="Day", variable=self.date_type, value = 1).pack(anchor=W)
+        self.weekly_button = ttk.Radiobutton(self.root, text="Week", variable=self.date_type, value = 2).pack(anchor=W)
+        self.monthly_button = ttk.Radiobutton(self.root, text="Month", variable=self.date_type, value = 3).pack(anchor=W)
+        self.year_button = ttk.Radiobutton(self.root, text="Year", variable=self.date_type, value = 4).pack(anchor=W)
+
+        # Entry
+        self.date_label = Label(self.root, text="Date (MM-DD-YY):", pady=10).pack(anchor=W)
+        self.date_entry = ttk.Entry(self.root, textvariable=self.date_text).pack(anchor=W)
 
         # Blank label
-        self.empty_label = Label(self.root, text="")
-        self.empty_label.pack()
+        self.empty_label = Label(self.root, text="").pack(anchor=W)
 
         # Create button
-        self.display = ttk.Button(self.frame, text="Create", style="TButton", command=self.generate_graphs)
-        self.display.pack()
-
-        # Finalize window
-        self.frame.pack()
+        self.display = ttk.Button(self.root, text="Create", style="TButton", command=self.generate_graphs).pack(anchor=W)
+        #self.display = Button(self.frame, text="Generate", command=self.generate_graphs).pack(anchor=W)
 
         print("> Successfully created GUI")
         print("> Waiting for input")
 
-        # Wait for input
-        self.frame.mainloop()
+        self.root.mainloop()
 
 
     def generate_graphs(self):
-        print("The following graphs were selected: ", end='')
+        print("> The following graphs were selected: ", end='')
         for graph in range(6):
             if self.graphs[graph].get():
                 self.selected.append(graph)
                 print(graph, end=' ')
-        self.frame.mainloop()
+        print("\n> Generating graphs, please wait")
 
+        #TODO: Call dataAnalysis on selected graphs and pass in selected widgets + date as parameters
+
+        # Close GUI after creation
+        self.root.destroy()
 
     # Returns list of selected graphs
     def get_graphs(self):
         return self.selected
+
+app = GUI()
